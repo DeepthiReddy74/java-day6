d5932c2f0fe82358843dd704462c91099d652e44
DeepthiReddy74 <reddydeepthi68699@gmail.com>
2024-12-07 14:13:48 +0530

    Add files via upload

---
+++ Arrayoperation.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Arrayoperation
+{
+    public static void main(String args[])
+    {
+        //initilization of array
+        int arr[]={22,44,565,789,74};
+        System.out.println(Arrays.toString(arr));
+        //accessing of array elements
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        //modification of an array
+        arr[2]=456;
+        arr[3]=345;
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
+
+    
---
+++ Break.java
@@ -0,0 +1,13 @@
+class Break
+{
+    public static void main(String args[])
+    {
+        System.out.println("break example");
+        for(int i=0;i<5;i++)
+        {
+            if(i==2)
+            break;
+            System.out.println(i);
+        }
+        }
+    }
